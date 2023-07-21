31387f928e29e9206672714cfebdc98c5f750b45
vaibhavrpatil9 <107748542+vaibhavrpatil9@users.noreply.github.com>
2023-07-21 15:30:26 +0530

    Update streamlit_app.py
    etxtra notes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header("Breakfast Menu")
+streamlit.text("Poha , wada and misal")
+streamlit.text("i am full of stomach")
