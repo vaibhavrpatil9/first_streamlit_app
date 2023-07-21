23757d795a8ab4e592aa22d3f6eb6a24e17d064c
vaibhavrpatil9 <107748542+vaibhavrpatil9@users.noreply.github.com>
2023-07-21 15:44:57 +0530

    Update streamlit_app.py
    new add

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.title('My Mom New Healthy Dinner')
 
 streamlit.header('Breakfast Favourite')
@@ -7,4 +9,5 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.dataframe(my_fruit_list)
 
