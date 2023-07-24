d8dbe72f4e6495e4b835d58877f333dd679add0d
vaibhavrpatil9 <107748542+vaibhavrpatil9@users.noreply.github.com>
2023-07-24 15:25:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import snowflake.connector
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.title('My Mom New Healthy Dinner')
